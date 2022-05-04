ed5d33b5d39b57969f79d959c3bfb64f77415c1f
khyunchoi <khyunchoi@naver.com>
2022-05-05 00:46:25 +0900

    ADD [Boj]

---
+++ Boj/python/11286.py
@@ -0,0 +1,16 @@
+import heapq
+import sys
+
+heap_list = []
+
+N = int(sys.stdin.readline())
+for _ in range(N):
+    x = int(sys.stdin.readline())
+
+    if x == 0:
+        if heap_list:
+            print(heapq.heappop(heap_list)[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap_list, (abs(x), x))
